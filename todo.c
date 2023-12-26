69904e8d4785a6534901ce68bacf60732135ca0b
Ttunay <tigrus232323k@gmail.com>
2023-12-26 16:36:16 +0300

    todo 1.3 DeleteTaks

--- todo.c
+++ todo.c
@@ -5,28 +5,61 @@
 void AddTask(char tasks[][100]) {
 printf("add task\n");
     int i = 0;  
-    do {
+    
+  do {
         scanf("%s", tasks[i]);
         i++;
-    } while (strcmp(tasks[i - 1], "exit") != 0); // Ограничение на количество вводимых задач
+    } while (strcmp(tasks[i - 1], "exit") != 0);
 
     printf("Your tasks:\n");
     for (int j = 0; j < i - 1; j++) {
         printf("%s\n", tasks[j]);
+        main();
     }
+
+    free(tasks);
+}
+void DeleteTask(char tasks[][100], int *taskCount) {
+    char *taskToDelete;
+    int i;
+
+    printf("Введите задачу, которую вы хотите удалить: ");
+    scanf("%s", taskToDelete);
+
+
+    for (i = 0; i < 100; i++) {
+        if (strcmp(tasks[i], taskToDelete) == 0) {
+            // Находим задачу, которую нужно удалить
+            break;
+        }
+    }
+
+    if (i == 100) {
+        // Задача не найдена
+        printf("Задача не найдена.\n");
+        return;
+    }
+
+    // Удаляем задачу из массива
+    for (; i < *taskCount - 1; i++) {
+        strcpy(tasks[i], tasks[i + 1]);    }
+
+    // Уменьшаем количество задач после удаления
+    (*taskCount--);
+
+    // Завершаем массив нулем
+    tasks[99][0] = '\0';
 }
 
-void DeleteTask() {
-
-}
 
 void EditTask() {
-    printf("Hello, Edit task!\n");
+    
 }
 
 int main(void) {
     char user[10];
-    char tasks[100][100];  // Массив для хранения задач
+    char tasks[100][100]; 
+    int taskCount = 0;
 
     printf("Hello, what do you want?\n");
     printf("Todo, Delete, Edit, Exit\n");
@@ -36,7 +69,7 @@ int main(void) {
         AddTask(tasks);
     } 
     else if (strcmp(user, "Delete") == 0) {
-        DeleteTask();
+        DeleteTask(tasks,&taskCount);
     } 
     else if (strcmp(user, "Edit") == 0) {
         EditTask();
