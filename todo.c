5aafa5857843772e2bc3425289c2c73297a50e95
Ttunay <tigrus232323k@gmail.com>
2023-12-26 18:30:51 +0300

    todo 1.4 add struct

--- todo.c
+++ todo.c
@@ -2,33 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
-void AddTask(char tasks[][100]) {
+
+  struct todo
+ {
+    char tasks[100][100];
+    int taskCount;
+    char *taskToDelete;
+ };
+ 
+
+void AddTask(struct todo * Todo) {
 printf("add task\n");
     int i = 0;  
     
   do {
-        scanf("%s", tasks[i]);
+        scanf("%s", Todo->tasks[i]);
         i++;
-    } while (strcmp(tasks[i - 1], "exit") != 0);
+    } while (strcmp(Todo->tasks[i - 1], "exit") != 0);
 
     printf("Your tasks:\n");
     for (int j = 0; j < i - 1; j++) {
-        printf("%s\n", tasks[j]);
-        main();
+        printf("%s\n", Todo->tasks[j]);
+    }
+    main();
+    
+}
+
+void DeleteTask() {
+
+
+    struct todo * Todo;
+    
+
+
+   printf("Your tasks:\n");
+    for (int j = 0; j < Todo->taskCount; j++) {
+        printf("%d. %s\n", j + 1, Todo->tasks[j]);
     }
 
-    free(tasks);
-}
-void DeleteTask(char tasks[][100], int *taskCount) {
-    char *taskToDelete;
+
+
     int i;
 
     printf("Введите задачу, которую вы хотите удалить: ");
-    scanf("%s", taskToDelete);
+    scanf("%s", Todo->taskToDelete);
 
 
     for (i = 0; i < 100; i++) {
-        if (strcmp(tasks[i], taskToDelete) == 0) {
+        if (strcmp(Todo->tasks[i], Todo->taskToDelete) == 0) {
             // Находим задачу, которую нужно удалить
             break;
         }
@@ -41,14 +62,11 @@ void DeleteTask(char tasks[][100], int *taskCount) {
     }
 
     // Удаляем задачу из массива
-    for (; i < *taskCount - 1; i++) {
-        strcpy(tasks[i], tasks[i + 1]);    }
+    for (; i < Todo->taskCount - 1; i++) {
+        strcpy(Todo->tasks[i], Todo->tasks[i + 1]);    }
 
     // Уменьшаем количество задач после удаления
-    (*taskCount--);
-
-    // Завершаем массив нулем
-    tasks[99][0] = '\0';
+    (Todo->taskCount--);
 }
 
 
@@ -74,7 +92,7 @@ int main(void) {
     else if (strcmp(user, "Edit") == 0) {
         EditTask();
     } 
-    else if (strcmp(user, "Exit") == 0){}
+    else if (strcmp(user, "Exit") == 0) {}
     
     else {
         printf("вы ввели чтото неправильное");
